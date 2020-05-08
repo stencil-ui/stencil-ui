ed36ab91805718e7def97fdea018495d57bdc2fc
devdigital <devdigital.cloud@gmail.com>
2020-05-08 15:46:39 +0100

    feat: add cover (#6)

---
+++ packages/layout/src/Cover.stories.js
@@ -0,0 +1,93 @@
+/** @jsx jsx */
+import { jsx } from '@theme-ui/core'
+import { Cover } from './Cover'
+import { BorderBox, Paragraphs, Words } from './utils'
+
+export default { title: 'Cover' }
+
+const defaultProps = {
+  children: (
+    <BorderBox>
+      <Paragraphs />
+    </BorderBox>
+  ),
+}
+
+export const withDefault = () => <Cover {...defaultProps} />
+
+export const withTop = () => (
+  <Cover
+    {...defaultProps}
+    top={
+      <BorderBox>
+        <Words number={1} />
+      </BorderBox>
+    }
+  />
+)
+
+export const withBottom = () => (
+  <Cover
+    {...defaultProps}
+    bottom={
+      <BorderBox>
+        <Words number={1} />
+      </BorderBox>
+    }
+  />
+)
+
+export const withTopAndBottom = () => (
+  <Cover
+    {...defaultProps}
+    top={
+      <BorderBox>
+        <Words number={1} />
+      </BorderBox>
+    }
+    bottom={
+      <BorderBox>
+        <Words number={1} />
+      </BorderBox>
+    }
+  />
+)
+
+export const withSpace = () => (
+  <Cover
+    {...defaultProps}
+    space="2rem"
+    top={
+      <BorderBox>
+        <Words number={1} />
+      </BorderBox>
+    }
+    bottom={
+      <BorderBox>
+        <Words number={1} />
+      </BorderBox>
+    }
+  />
+)
+
+export const withMinHeight = () => (
+  <Cover
+    space="2rem"
+    minHeight="200px"
+    top={
+      <BorderBox>
+        <Words number={1} />
+      </BorderBox>
+    }
+    children={
+      <BorderBox>
+        <Words number={1} />
+      </BorderBox>
+    }
+    bottom={
+      <BorderBox>
+        <Words number={1} />
+      </BorderBox>
+    }
+  />
+)
