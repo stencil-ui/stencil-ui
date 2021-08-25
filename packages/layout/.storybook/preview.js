0cfc8f4a5408274be173cf8de3d6230b25a14e6d
devdigital <devdigital.cloud@gmail.com>
2021-08-25 16:37:21 +0100

    wip: update file names

--- packages/layout/.storybook/preview.js
+++
@@ -1,8 +0,0 @@
-import { addDecorator } from '@storybook/react'
-import { withPulse } from 'storybook-addon-pulse'
-
-addDecorator(withPulse)
-
-// addDecorator(storyFn => (
-//   <ThemeProvider theme={theme}>{storyFn()}</ThemeProvider>
-// ))
